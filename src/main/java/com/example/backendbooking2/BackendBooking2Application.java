d3918586fe8ea75c916d40a2a527903e0cd26722
moha3052 <moha3052@hotmail.dk>
2024-11-17 19:24:31 +0100

    ny

---
+++ src/main/java/com/example/backendbooking2/BackendBooking2Application.java
@@ -0,0 +1,13 @@
+package com.example.backendbooking2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBooking2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendBooking2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendbooking2/BackendBooking2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendbooking2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBooking2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
