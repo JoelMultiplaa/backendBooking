84804815de40dbcadb1b50fef84ca25f0353a37f
Lenovo <joda0002@st>
2024-11-27 12:47:16 +0100

    små fix

--- src/main/java/com/example/backendbooking2/Repository/CustomerRepository.java
+++ src/main/java/com/example/backendbooking2/Repository/CustomerRepository.java
@@ -1,10 +1,15 @@
 package com.example.backendbooking2.Repository;
 
+import com.example.backendbooking2.Entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Repository
 
-public interface CustomerRepository extends JpaRepository<Service, String> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+
 }
