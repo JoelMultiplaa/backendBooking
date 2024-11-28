0485a33d3dc1224569ed64d4cc543989b5c66c1a
moha3052 <moha3052@hotmail.dk>
2024-11-28 15:49:56 +0100

    Product og orderline er færdig

--- src/main/java/com/example/backendbooking2/Controller/ProductController.java
+++ src/main/java/com/example/backendbooking2/Controller/ProductController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @CrossOrigin("*")
 @RequestMapping("api/v1")
 public class ProductController {
-    private ProductService productService;
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
